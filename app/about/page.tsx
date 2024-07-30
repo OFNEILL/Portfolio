584747b96fd2de9caf8ca88b47c29e991535f48d
Joseph O’Neill <Joseph.ONeill@jeffreyai.com>
2024-07-30 11:22:44 +0100

    image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
 export default function About() {
-  return <div className="flex flex-col justify-center">yay</div>;
+  return (
+    <div>
+      <div className="aspect-square h-full max-h-[600px] overflow-hidden">
+        <Image
+          src="/my-pic.jpg"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="object-cover w-full h-full"
+          alt="cool-ass photo of me"
+        />
+      </div>
+    </div>
+  );
 }
